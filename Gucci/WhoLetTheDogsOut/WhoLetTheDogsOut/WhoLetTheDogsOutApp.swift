b6604588119597fd104ec5fc9e80415020c38bc9
Damagucci-Juice <jsr9874@gmail.com>
2023-12-18 20:47:36 +0900

    init: Gucci's Project Start

---
+++ Gucci/WhoLetTheDogsOut/WhoLetTheDogsOut/WhoLetTheDogsOutApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoLetTheDogsOutApp.swift
+//  WhoLetTheDogsOut
+//
+//  Created by Gucci on 12/18/23.
+//
+
+import SwiftUI
+
+@main
+struct WhoLetTheDogsOutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
