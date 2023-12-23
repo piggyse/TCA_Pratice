2b2d65ec43a3a7903971c5406950dc5f279f903d
P_SeoB <akdlsjtm21@naver.com>
2023-12-23 19:26:28 +0900

    remove print

--- TCA_ImageList/TCA_ImageList/ImageListViewFeature.swift
+++ TCA_ImageList/TCA_ImageList/ImageListViewFeature.swift
@@ -28,21 +28,16 @@ struct ImageListViewFeature: Reducer {
         case .fetchImageMetaDataArray:
             state.isLoadingImageMetaDataArray = true
             return .run { send in
-                print("fetchImageMetaDataArray Action start")
                 let imageMetaDataArray = await imageUseCase.requestImageMetaDataList(size: 10) ?? []
                 await send(.imageMetaDataResponse(imageMetaDataArray))
-                print("fetchImageMetaDataArray Action End")
             }
         case .fetchMoreImageMetaData(let size):
             return .run { send in
-                print("fetchMoreImageMetaDataArray Action start")
                 let imageMetaDataArray = await imageUseCase.requestImageMetaDataList(size: size) ?? []
                 await send(.moreImageMetaDataResponse(imageMetaDataArray))
             }
         case .imageMetaDataResponse(let imageMetaData):
-            print("imageMetaData Response Action Start")
             state.imageMetaDataArray = imageMetaData
-            print("imageMetaData Response Action End")
             state.isLoadingImageMetaDataArray = false
             return .none
 
