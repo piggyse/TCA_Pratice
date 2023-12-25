7c4c81fbd2474fe9a8b8faee3ee1e8ed30d8f1b3
Damagucci-Juice <jsr9874@gmail.com>
2023-12-25 11:10:24 +0900

    feat: 이미지를 버튼을 눌러서 불러오는 뷰 작업

--- Gucci/WhoLetTheDogsOut/WhoLetTheDogsOut/ContentView.swift
+++ Gucci/WhoLetTheDogsOut/WhoLetTheDogsOut/ContentView.swift
@@ -8,12 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var url = URL(string: "https://picsum.photos/200")
+    @State private var id = UUID()
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            AsyncImage(url: url) { asyncImagePhase in
+                switch asyncImagePhase {
+                case .empty:
+                    Color(.white)
+                        .frame(width: 300, height: 400)
+                        .overlay {
+                            ProgressView()
+                                .progressViewStyle(CircularProgressViewStyle(tint: .orange))
+                                .scaleEffect(3)                            
+                        }
+                case .success(let image):
+                    image
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .frame(width: 300, height: 400, alignment: .center)
+                case .failure(let error):
+                    Text(error.localizedDescription)
+                @unknown default:
+                    Color(.black)
+                        .overlay {
+                            Text("Something gose wrong")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                        }
+                }
+            }
+            .id(id)
+            
+            Spacer()
+            
+            Button {
+                id = UUID()
+            } label: {
+                Text("이미지 불러오기")
+            }
+            .buttonStyle(.borderedProminent)
         }
         .padding()
     }
